7ac0433ee43b21618d1453ea672f4da9ea674ff4
NexusObsess <thenexusobsession@gmail.com>
2026-08-03 14:20:46 +1200

    Environment Exit Door, Silo room, Fixed Stairs
    Now in Grey boxing 1 to avoid contradictions

--- Assets/Scenes/Nicole/Animation Collider.cs
+++ Assets/Scenes/Nicole/Animation Collider.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using System.Collections;
+using static Unity.VisualScripting.Member;
 
 public class AnimationCollider : MonoBehaviour
 {
     [SerializeField] GameObject animatedobject;
     [SerializeField] Animator anim;
+
+    [SerializeField] bool Exit = false;
+    [SerializeField] Menus menus;
+
+    AudioSource source; // nickname for the text sound effect
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>(); // assigns as the audiosource from the game object the script is on
+    }
 
     public void OnTriggerEnter()
     {
@@ -14,6 +26,24 @@ public class AnimationCollider : MonoBehaviour
         return;
         anim.SetBool("In", false);
         anim.SetBool("Out", true);
+
+        OpenAudio();
+
+        if (Exit)
+        {
+            Debug.Log("Exit condition");
+            StartCoroutine(ExitSequence());
+        }
+        else
+        {
+            Debug.Log("Random door");
+        }
+    }
+
+    public IEnumerator ExitSequence()
+    {
+        yield return new WaitForSeconds(1f);
+        menus.NextScene();
     }
 
     public void OnTriggerExit()
@@ -24,5 +54,19 @@ public class AnimationCollider : MonoBehaviour
         return;
         anim.SetBool("In", true);
         anim.SetBool("Out", false);
+
+        OpenAudio();
+    }
+
+    public void OpenAudio()
+    {
+        if (source != null && source.clip != null) // checks if audio source was on game object and if it has an audio clip attached
+        {
+            source.Play();
+        }
+        else // debugging
+        {
+            Debug.LogWarning("No audio source on game object");
+        }
     }
 }
