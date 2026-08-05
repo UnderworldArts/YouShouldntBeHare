cf80fac4f4cceb8911acfe33aff7e4b4aad90b40
angievv0 <angelinabmx@gmail.com>
2026-08-05 21:11:46 +1200

    Basic Dialogue system (not added yet)

---
+++ Assets/Scenes/Angelina/HareDialogue.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HareDialogue : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("Interact");
+    }
+}
